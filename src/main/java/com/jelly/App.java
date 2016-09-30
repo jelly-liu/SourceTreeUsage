a8d964cc6fbb13181e222267d43ade05b7dd5c91
jianli.liu <jianli.liu@thistech.com>
2016-09-30 11:07:54 +0800

    Revert "test uncommit"
    This reverts commit 267cadfc3f540079022eaf183b8cc6003a67613e.

--- src/main/java/com/jelly/App.java
+++ src/main/java/com/jelly/App.java
@@ -6,6 +6,5 @@ package com.jelly;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello SourceTreeUsage!");
-        System.out.println("Hello SourceTreeUsage!");
     }
 }
